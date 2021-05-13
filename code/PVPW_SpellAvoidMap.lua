16e5f3496c692ecfbb7bd85d4dda2b4f667c113e
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2021-05-14 00:07:39 +0200

    Fix hammer of justice and wrath * Mixed up hammer of wrath which can miss but not resist with hammer of justice which can resist but not miss

--- code/PVPW_SpellAvoidMap.lua
+++ code/PVPW_SpellAvoidMap.lua
@@ -671,9 +671,9 @@ else
         ["dodge"] = false,
         ["parry"] = false,
         ["immune"] = true,
-        ["miss"] = false,
+        ["miss"] = true,
         ["block"] = false,
-        ["resist"] = true,
+        ["resist"] = false,
         ["self_avoid"] = true,
         ["enemy_avoid"] = true
       },
@@ -685,9 +685,9 @@ else
         ["dodge"] = false,
         ["parry"] = false,
         ["immune"] = true,
-        ["miss"] = true,
+        ["miss"] = false,
         ["block"] = false,
-        ["resist"] = false,
+        ["resist"] = true,
         ["self_avoid"] = true,
         ["enemy_avoid"] = true
       }
